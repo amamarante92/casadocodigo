0178c4fa8e2970c0a9bdd899fcbfc2f02dfcee27
André Marques <amamarante92@hotmail.com>
2019-08-05 17:52:10 -0300

    Iniciando segunda parte do curso. Aprimorando layout

--- README.md
+++
@@ -1,3 +0,0 @@
-casadocodigo
-
-Projeto feito para o curso básico de Node.js da escola de código online Alura
--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -1,6 +1,33 @@
-class LivroDAO {
+class LivroDao {
+
     constructor(db) {
         this._db = db;
+    }
+
+    adiciona(livro) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                INSERT INTO livros (
+                    titulo, 
+                    preco,
+                    descricao
+                ) values (?,?,?)
+                `,
+                [
+                    livro.titulo,
+                    livro.preco,
+                    livro.descricao
+                ],
+                function (err) {
+                    if (err) {
+                        console.log(err);
+                        return reject('Não foi possível adicionar o livro!');
+                    }
+
+                    resolve();
+                }
+            )
+        });
     }
 
     lista() {
@@ -13,37 +40,56 @@ class LivroDAO {
                     return resolve(resultados);
                 }
             )
-
         });
     }
 
-    adiciona(livro) {
+    buscaPorId(id) {
+
         return new Promise((resolve, reject) => {
-            this._db.run(`
-            INSERT INTO LIVROS (
-                    titulo,
-                    preco,
-                    descricao
-                ) values (?, ?, ?)
-            `, [
-                livro.titulo,
-                livro.preco,
-                livro.descricao
-                ],
-                function(err){
-                    if(err){
-                        console.log(err);
-                        return reject('Não foi possível adicionar o livro');
+            this._db.get(
+                `
+                    SELECT *
+                    FROM livros
+                    WHERE id = ?
+                `,
+                [id],
+                (erro, livro) => {
+                    if (erro) {
+                        return reject('Não foi possível encontrar o livro!');
                     }
-
-
-                    resolve();
+                    return resolve(livro);
                 }
             );
         });
     }
 
+    atualiza(livro) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                UPDATE livros SET
+                titulo = ?,
+                preco = ?,
+                descricao = ?
+                WHERE id = ?
+            `,
+            [
+                livro.titulo,
+                livro.preco,
+                livro.descricao,
+                livro.id
+            ],
+            erro => {
+                if (erro) {
+                    return reject('Não foi possível atualizar o livro!');
+                }
+
+                resolve();
+            });
+        });
+    }
+
     remove(id) {
+
         return new Promise((resolve, reject) => {
             this._db.get(
                 `
@@ -61,46 +107,6 @@ class LivroDAO {
             );
         });
     }
-
-
-    buscaPorId(id){
-        return new Promise((resolve, reject) =>{
-            this._db.get(
-                'SELECT * FROM livros WHERE id = ?',
-                [id],
-                (erro, resultado) =>{
-                    if(erro){
-                        return reject('impossível buscar o livro');
-                    }
-                    return resolve(resultado);
-                }
-            )
-        });
-    }
-
-    atualiza (livro){
-        return new Promise((resolve, reject) => {
-            this._db.run(
-                `UPDATE livros
-                SET titulo = ?,
-                preco = ?,
-                descricao = ?
-                WHERE id = ?`,
-                [
-                    livro.titulo,
-                    livro.preco,
-                    livro.descricao,
-                    livro.id
-                ],
-                (erro) =>{
-                    if(erro){
-                        return reject('não foi possível atualizar');
-                    }
-                    return resolve();
-                }
-            )
-        });
-    }
 }
 
-module.exports = LivroDAO;
+module.exports = LivroDao;
